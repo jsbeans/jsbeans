57736b862643fcf6ebc90a4d2878c14ab6921940
Dmitry Abramov <dimonab@gmail.com>
2016-10-25 23:35:11 +0300

    jsbeans: antiplag: workspace->plugin

---
+++ projects/antiplag/workspace/src/main/java/ru/avicomp/ontoed/ws/WorkspacePluginActivator.java
@@ -0,0 +1,25 @@
+/*
+ * This file is the part of jsBeans, high-level JavaScript client-server framework.
+ * The contents of this file are subject to the MIT License (MIT).
+ * (c) aa@sis.ru, da@sis.ru, Special Information Systems, LLC, 2011-2016
+ *
+ * Настоящий файл является частью клиент-сервеной JavaScript платформы.
+ * Условия использования и распространения содержимого данного файла соответствуют программному обеспечению с открытыми исходными кодами и равнозначно MIT License (MIT).
+ * Авторские права принадлежат aa@sis.ru, da@sis.ru, ООО СИС, 2011-2016гг.
+ */
+
+package ru.avicomp.ontoed.ws;
+
+import org.jsbeans.helpers.ConfigHelper;
+import org.jsbeans.plugin.PluginActivator;
+
+public class WorkspacePluginActivator implements PluginActivator {
+
+    @Override
+    public void init() {
+        String path = ConfigHelper.getPluginHomeFolder(this) + "/" + ConfigHelper.getConfigString("workspace.folder");
+        ConfigHelper.addWebFolder(path);
+        ConfigHelper.addJssFolder(path);
+    }
+
+}
