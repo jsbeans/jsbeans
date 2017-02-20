e3407d5552ea1ec5cd341e5bc7bee659bd71d918
Dmitry <da@sis.ru>
2017-02-20 18:58:04 +0300

    Added JsonHelper class for convert NattiveObject->JsonObject

---
+++ kernel/src/main/java/org/jsbeans/helpers/JsonHelper.java
@@ -0,0 +1,43 @@
+package org.jsbeans.helpers;
+
+import org.jsbeans.types.JsonArray;
+import org.jsbeans.types.JsonObject;
+import org.jsbeans.types.JsonPrimitive;
+import org.mozilla.javascript.ConsString;
+import org.mozilla.javascript.NativeArray;
+import org.mozilla.javascript.NativeObject;
+import org.mozilla.javascript.Scriptable;
+
+public class JsonHelper {
+
+    public static Object nativeToJsonObject(Object obj){
+        if (obj == null) {
+            return null;
+        } else if (obj instanceof NativeObject) {
+            NativeObject nobj = (NativeObject) obj;
+            JsonObject json = new JsonObject();
+            for (Object propId : NativeObject.getPropertyIds(nobj)){
+                String key = propId.toString();
+                Object value = NativeObject.getProperty(nobj, key);
+                json.put(key, nativeToJsonObject(value));
+            }
+            return json;
+        } else if (obj instanceof NativeArray) {
+            NativeArray array = (NativeArray) obj;
+            JsonArray json = new JsonArray();
+            for (Object element:array.getIds()){
+                int index = (Integer) element;
+                json.add(nativeToJsonObject((Scriptable) array.get(index, null)));
+
+            }
+            return json;
+        } else if (obj instanceof String || obj instanceof ConsString) {
+            return obj.toString();
+        } else if (obj instanceof Boolean || obj instanceof Number) {
+            return obj;
+        } else {
+            String strValue = obj.toString();
+            return strValue;
+        }
+    }
+}
