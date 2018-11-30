f0af570a5fdac02c8dc64327b44113a329f9534c
Alexander Alexeyev <aa@sis.ru>
2018-11-30 12:05:35 +0300

    Large files download bugfix

--- plugins/web/src/main/java/org/jsbeans/web/HttpJsbServlet.java
+++ plugins/web/src/main/java/org/jsbeans/web/HttpJsbServlet.java
@@ -216,6 +216,9 @@ public class HttpJsbServlet extends HttpServlet {
 
     private UpdateStatusMessage execCmd(String beanPath, String proc, String params, String session, String clientAddr, String user, String rid, String uri, String token, HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
         Timeout timeout = ActorHelper.getServiceCommTimeout();
+        if(beanPath.endsWith("download.jsb")){
+        	timeout = ActorHelper.getInfiniteTimeout();
+        }
         ExecuteScriptMessage msg = new ExecuteScriptMessage(String.format("(function(){ var Web = JSB.getInstance('JSB.Web'); var result = JSB.getInstance('JSB.HttpJsb').exec('%s','%s', [%s, decodeURIComponent('%s')]); var opts = {}; if(result instanceof Web.Response){opts = result.opts; result = result.data; } return {exec: result, opts: opts};})()", beanPath, proc, params, URLEncoder.encode(uri, "UTF-8")), false);
         msg.setUserToken(token);
         msg.setScopePath(session);
