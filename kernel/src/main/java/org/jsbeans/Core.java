67e7cfb11a6a934b4d9f57f3d55bd8c9ccebb616
Alexander Alexeyev <aa@sis.ru>
2018-12-26 20:21:58 +0300

    Core bugfix

--- kernel/src/main/java/org/jsbeans/Core.java
+++ kernel/src/main/java/org/jsbeans/Core.java
@@ -33,6 +33,7 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.UUID;
