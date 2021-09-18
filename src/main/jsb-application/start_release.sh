601d080023e87e4bc95cd90ecb565b05ecf00a9c
Dmitry Abramov <da@sis.ru>
2021-09-18 19:31:42 +0300

    rebuild

--- src/main/jsb-application/start_release.sh
+++ src/main/jsb-application/start_release.sh
@@ -5,5 +5,7 @@ cd $APP_DIR
 
 JARS="$(cat .classpath_jars |sed 's/\\/\//g')"
 
+#fix
+
 mkdir -p logs
 java -XX:+UseConcMarkSweepGC -XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled -Xms512m -Xmx8192m -Dfile.encoding=UTF-8 "$@" -classpath config:$JARS org.jsbeans.Starter
