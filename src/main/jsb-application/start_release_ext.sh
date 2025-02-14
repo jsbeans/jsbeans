69a1413c847ea653c6136780ea970daabaeeee20
Dmitry Abramov <dimonab@gmail.com>
2025-02-14 16:04:07 +0300

    * [DEV] убраны версии из названий jar при assembly проекта

---
+++ src/main/jsb-application/start_release_ext.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+export APP_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $APP_DIR
+
+JARS="$(find . -name "*.classpath" -print0 | sort | xargs -0 cat | sed 's/$/:/' | tr -d '\n' | sed 's/\\/\//g')"
+
+if [[ -f version.txt ]]; then
+  echo "build.version=\"$(cat version.txt)\"" > config/version.conf
+fi
+
+if [[ -d logs ]]; then
+  bklog="./logs/$(date '+%Y-%m-%d %H:%M:%S')/"
+  mkdir -p "$bklog"
+  find ./logs/ -maxdepth 1 -type f -exec cp {} "$bklog" \;
+else
+  mkdir -p logs
+fi
+
+LANG="en_US.UTF-8"
+set -x
+java -DBUILD_VERSION="$(cat version.txt 2>/dev/null||echo)"\
+  -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 \
+  "${@}" -classpath config:$JARS org.jsbeans.Starter
