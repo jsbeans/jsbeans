6d08c0bb161adc2c2372f6945a84664387eb42f3
Dmitry Abramov <dimonab@gmail.com>
2025-03-27 18:01:41 +0300

    * [DEV] исправление запуска в start_debug.sh

--- src/main/jsb-application/start_debug.sh
+++ src/main/jsb-application/start_debug.sh
@@ -3,16 +3,23 @@
 export APP_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $APP_DIR
 
-JARS="$(cat classpath_jars |sed 's/\\/\//g')"
+JARS=""
+for file in *.classpath; do
+  if [[ -n "$JARS" ]]; then
+      JARS+=":"
+  fi
+  JARS+=$(cat $file)
+done
 
-
-if [[ -d ./logs ]]; then
-  bklog="./logs/$(date '+%Y-%m-%d %H:%M:%S')/"
+if [[ -d logs ]]; then
+  bklog="./logs/$(date '+%Y-%m-%d_%H%M%S')/"
   mkdir -p "$bklog"
   find ./logs/ -maxdepth 1 -type f -exec cp {} "$bklog" \;
 else
   mkdir -p logs
 fi
+
+LANG="en_US.UTF-8"
 
 # -XX:+UseConcMarkSweepGC -XX:+CMSPermGenSweepingEnabled
 java \
